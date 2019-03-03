ca15e7fa28e39d1f9480eb8f526ae7335ae03aad
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-03 12:17:11 -0800

    lab9

--- menu.cpp
+++ menu.cpp
@@ -1,5 +1,15 @@
-//
-// Created by Sandro on 3/3/2019.
-//
-
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            March 3, 2019
+** Description:     Menu class holds the main menu and user prompts
+**                  for the program. The menu class also holds the
+**                  user input validation function.
+*********************************************************************/
 #include "menu.hpp"
+
+void Menu::mainMenu() {
+    cout << "Welcome, the following tasks will be performed\n";
+    cout << ">> Simple Search\n";
+    cout << ">> Sorting\n";
+    cout << ">> Binary Search\n";
+}
--- menu.hpp
+++ menu.hpp
@@ -1,8 +1,33 @@
-//
-// Created by Sandro on 3/3/2019.
-//
+/*********************************************************************
+** Author:          Sandro Aguilar
+** Date:            March 3, 2019
+** Description:     Menu class holds the main menu and user prompts
+**                  for the program. The menu class also holds the
+**                  user input validation function.
+*********************************************************************/
+#ifndef MENU_HPP
+#define MENU_HPP
 
-#ifndef LAB9_MENU_HPP
-#define LAB9_MENU_HPP
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+using std::string;
+using std::setw;
+using std::left;
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Menu {
+public:
+    void mainMenu();
+    int validateNumber(int min, int max);
+
+private:
+
+};
+
 
 #endif //LAB9_MENU_HPP
